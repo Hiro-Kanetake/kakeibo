16162527e11b43872bebcedd41619441aa4d2504
Hiro-Kanetake <hiro_k@live.com>
2022-11-09 22:37:58 +0900

    feat: add mock data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 450,
+      date: new Date(2022, 9, 14),
+    },
+    { 
+      id: "e2",
+      title: "Rice",
+      amount: 1000,
+      date: new Date(2022, 9, 20)
+    },
+    {
+      id: "e3",
+      title: "Electricity Bill",
+      amount: 8000,
+      date: new Date(2022, 9, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 20000,
+      date: new Date(2022, 10, 1),
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Expenses items={expenses} />
       </header>
     </div>
   );
